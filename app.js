af9ee7e4f38708003f7749decdd6f9953c48bc19
TintinCoder <Divyamyadav40@gmail.com>
2023-05-16 17:07:32 +0530

    Second Commit - Adding the routes(about and home)

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res)=>{
     res.send("Hello VidJot")
 })
 
+app.get('/about', (req, res)=>{
+    res.send('Welcome to the about page!!\n VidJot is used to jot your video ideas on the web')
+})
+
 const port = 5000;
 
 app.listen(port, ()=>{
